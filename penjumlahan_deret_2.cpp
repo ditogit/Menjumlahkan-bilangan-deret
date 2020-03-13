f1fb4792b17ece2cb433c54130c87ce56632ebcf
ditogit <amaddito@gmail.com>
2020-03-13 07:51:15 +0700

    Add files via upload

---
+++ penjumlahan_deret_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x,y=0,z;
+    
+    cout<<"Masukan bilangan bulat positif ";
+    cin>>z;
+    
+    if(z<=0){
+    	cout<<"Angka yang dimasukan tidak sesuai, ulangi lagi"; 
+	} else {
+	
+	    cout<<"Penjumlahan deret bilangan bulat positif dari "<< z << "\n";
+	    
+	    for (x=1; x<=z ;x++){
+	    y = y+x;
+	    cout<< x;
+	        if(x<z){
+	           cout<<"+"; 
+	        } 
+	    
+	    }
+	    cout<<" = "<< y; 
+	}
+    
+}
